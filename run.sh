becceb59694fdbb970e17d9ae0054d82b60945ca
JiuhaiChen <jiuhaichen@gmail.com>
2025-05-16 23:19:17 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -22,7 +22,7 @@ torchrun --nproc_per_node=8 \
     --mm_use_im_start_end False \
     --mm_use_im_patch_token False \
     --bf16 True \
-    --output_dir ${OUTPUT} \
+    --output_dir ${OUTPUT_FOLDER} \
     --num_train_epochs 1 \
     --per_device_train_batch_size 16 \
     --per_device_eval_batch_size 4 \
