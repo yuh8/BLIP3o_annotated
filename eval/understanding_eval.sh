7b0ba44ff6f22b6514ba4d0b23f219d79f781e7d
JiuhaiChen <jiuhaichen@gmail.com>
2025-05-22 18:55:51 -0700

    Update understanding_eval.sh

--- eval/understanding_eval.sh
+++ eval/understanding_eval.sh
@@ -4,7 +4,7 @@ python -m accelerate.commands.launch \
     --num_processes=8 \
     -m lmms_eval \
     --model blip3o \
-    --model_args pretrained="BLIP3o/BLIP3o-Model" \
+    --model_args pretrained="your/model/path/" \
     --tasks  mme \
     --batch_size 1 \
     --log_samples \
