cc10c11f1b248941e4045e39a712eb29ee0e9ffb
JiuhaiChen <jiuhaichen@gmail.com>
2025-05-22 18:21:07 -0700

    Delete blip3o/blip3o directory

--- blip3o/blip3o/__init__.py
+++
@@ -1,1 +0,0 @@
-from .model import blip3oLlamaForCausalLM
--- blip3o/blip3o/model/__init__.py
+++
@@ -1,5 +0,0 @@
-from .language_model.blip3o_llama import blip3oLlamaForCausalLM, blip3oConfig
-from .language_model.blip3o_qwen import blip3oQwenForCausalLM, blip3oQwenConfig
-from .language_model.blip3o_qwen_inference import blip3oQwenForInferenceLM, blip3oQwenConfig
-
-
